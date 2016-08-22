5741ff21618d41ca74485bf54617d5e38fb23e97
Amber Farrington and Kyle Lange <kyle@>
2016-08-22 10:47:37 -0700

    add initial javascript functionality with an object and a self-made method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,33 @@
 // Business Logic
+function Destinations(city) {
+  this.city = city;
+}
 
+Destinations.prototype.vacation = function (){
+  return this.city;
+}
 
 
 // User Interface Logic
+$(document).ready(function(){
+  $("#vacation").submit(function(event){
+    event.preventDefault();
+    debugger;
+    var cityInput = $("#cityInput").val();
+    var landmarInput = $("#landmarkInput").val();
+    var moneyInput = $("#moneyInput").val();
+    var returnInput = $("input:radio[name=goBack]:checked").val();
+
+    var newDestination = new Destinations(cityInput);
+
+    $("ul#placeInfo").append("<li><span class='city'>" + newDestination.vacation() + "</span></li>");
+
+    $("#placeInfo li").last().click(function(){
+      $("#showPlaceInfo").show();
+      $("#showPlaceInfo h2").text(newDestination.city);
+      $(".city").text(newDestination.city);
+    });
+
+      $("input#cityInput").val("");
+  });
+});
