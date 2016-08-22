bde4a50fc4648c47eb1946efbcb938dbb42f830c
Amber Farrington and Kyle Lange <amber@>
2016-08-22 12:00:06 -0700

    add the remaining variables to our object

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 // Business Logic
 
-function Destinations(_city, _landmark) {
+function Destinations(_city, _landmark, _money,_visit) {
   this.cityInput = _city;
   this.landInput = _landmark;
+  this.moneyInput = _money;
+  this.returnInput = _visit;
 }
 
 Destinations.prototype.vacation = function (){
   return this.cityInput;
   return this.landInput;
+  return this.moneyInput;
+  return this.returnInput;
 }
 
 
@@ -21,22 +25,25 @@ $(document).ready(function(){
     var moneyInput = $("#moneyInput").val();
     var returnInput = $("input:radio[name=goBack]:checked").val();
 
-    var newDestination = new Destinations(cityInput, landInput);
-    console.log(cityInput, landInput);
+    var newDestination = new Destinations(cityInput, landInput, moneyInput, returnInput);
+    console.log(cityInput, landInput, moneyInput, returnInput);
     console.log(newDestination);
 
-    $("ul#placeInfo").append("<li><span class='city'>" + newDestination.vacation() + "</span></li>");
+    $("ul#placeInfo").append("<li><span class='cityName'>" + newDestination.vacation() + "</span></li>");
 
     $("#placeInfo li").last().click(function(){
       $("#showPlaceInfo").show();
       $("#showPlaceInfo h2").text(newDestination.cityInput);
-      $("#showPlaceInfo h2").text(newDestination.landInput);
       $(".city").text(newDestination.cityInput);
       $(".landmarks").text(newDestination.landInput);
+      $(".money").text(newDestination.moneyInput);
+      $(".returnVisit").text(newDestination.returnInput);
     });
 
       $("input#cityInput").val("");
       $("input#landmarkInput").val("");
+      $("input#moneyInput").val("");
+      $("input#returnInput").val("");
 
   });
 });
