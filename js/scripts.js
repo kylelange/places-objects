b69fa389fbae49c7d2050fd60277e6209c6255d2
Amber Farrington and Kyle Lange <amber@>
2016-08-22 11:37:10 -0700

    add JS functionality for our second form field: Landmarks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,42 @@
 // Business Logic
-function Destinations(city) {
-  this.city = city;
+
+function Destinations(_city, _landmark) {
+  this.cityInput = _city;
+  this.landInput = _landmark;
 }
 
 Destinations.prototype.vacation = function (){
-  return this.city;
+  return this.cityInput;
+  return this.landInput;
 }
 
 
 // User Interface Logic
 $(document).ready(function(){
-  $("#vacation").submit(function(event){
+  $("form#vacation").submit(function(event){
     event.preventDefault();
     debugger;
     var cityInput = $("#cityInput").val();
-    var landmarInput = $("#landmarkInput").val();
+    var landInput = $("#landInput").val();
     var moneyInput = $("#moneyInput").val();
     var returnInput = $("input:radio[name=goBack]:checked").val();
 
-    var newDestination = new Destinations(cityInput);
+    var newDestination = new Destinations(cityInput, landInput);
+    console.log(cityInput, landInput);
+    console.log(newDestination);
 
     $("ul#placeInfo").append("<li><span class='city'>" + newDestination.vacation() + "</span></li>");
 
     $("#placeInfo li").last().click(function(){
       $("#showPlaceInfo").show();
-      $("#showPlaceInfo h2").text(newDestination.city);
-      $(".city").text(newDestination.city);
+      $("#showPlaceInfo h2").text(newDestination.cityInput);
+      $("#showPlaceInfo h2").text(newDestination.landInput);
+      $(".city").text(newDestination.cityInput);
+      $(".landmarks").text(newDestination.landInput);
     });
 
       $("input#cityInput").val("");
+      $("input#landmarkInput").val("");
+
   });
 });
